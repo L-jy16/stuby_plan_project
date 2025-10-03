f2e3cc1da34d4e245978616aeb4e4644f007e1af
이지영 <lee.jy@ijiyeong-ui-noteubug.local>
2025-10-04 00:58:13 +0900

    첫 커밋

---
+++ App/server/index.js
@@ -0,0 +1,9 @@
+/** @format */
+
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+
+const app = express();
+const port = 5050;
+const config = require;
